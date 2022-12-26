f0f55ff66e499bc5242abbdae7f879f9df43b1e8
Heba2h <99084258+Heba2h@users.noreply.github.com>
2022-12-26 02:50:25 +0200

    fix: removing-TB

--- testing.v
+++ testing.v
@@ -3,7 +3,7 @@ module uartRx (rxStart,rxD,rst,clk,rxData,store,clrRxStartBit);
   //rxD: Serial input from other peripherals
   //store:Signal to the APB interface block indicating that the reception is complete and to store the data
   //rxData:Data received.
-  //clrRxStartBit:Signal to APB interface block to clear r×Start
+  //clrRxStartBit:Signal to APB interface block to clear rÃ—Start
   // 1 start bit , 1 stop bit , 1 parity bit  --> Data
   // Reciver_reg :11-bit register 
   
@@ -121,46 +121,15 @@ module uartRx (rxStart,rxD,rst,clk,rxData,store,clrRxStartBit);
    //outputs of switch cases   
     assign rxCountFlag =(state==COUNT)?1:0;
     assign rxShiftFlag=(state==BIT_COUNT)?1:0;
-   assign rxBitCountFlag=(state==BIT_COUNT)?1:0;
-   assign clrRxStartBit=(state==CLEAR)?1:0;
-   assign store=(state==RECEIVED)?1:0;
-    assign rxData=rxShiftReg;
    
- endmodule 
+assign rxBitCountFlag=(state==BIT_COUNT)?1:0;
+   
+assign clrRxStartBit=(state==CLEAR)?1:0;
+   
+assign store=(state==RECEIVED)?1:0;
+    assign 
+rxData=rxShiftReg;
+   
  
-   module TB_Rx;  
-  input rxStart,rxD,rst,clk;
-  
-  //Outputs
-  output[10:0] rxData;
-  output store,clrRxStartBit;
-  
-  //flag signals 
-  wire rxCountFlag;
-  wire rxShiftFlag;
-  wire rxBitCountFlag 
-  
-  //internal registers 
-  reg[3:0] rxBitCount;
-  reg[3:0] counter16;
-  reg[10:0] rxShiftReg;
-  reg[2:0] state;
-  reg[2:0] nextState; 
-     initial begin
-       $monitor("x = %b , y = %b , z = %b",x,y,z);
-       x= 0 ; 
-       y = 0 ; 
-       #10;
-       x= 0 ; 
-       y = 1 ; 
-       #10;
-       x= 1 ; 
-       y = 0 ; 
-       #10;
-       x= 1 ; 
-       y = 1 ; 
-       #10;
-     end
-    uartRx aw(rxStart,rxD,rst,clk,rxData,store,clrRxStartBit);
-  endmodule;
-
+endmodule 
+ 
