d3443dd12965316ec0dd743d884788ec7f19b4c7
Mina Mounir <104218090+Mina-Mounir-Farid@users.noreply.github.com>
2023-01-03 16:45:17 +0200

    Update gpioWithWaitState.v

--- gpioWithWaitState.v
+++ gpioWithWaitState.v
@@ -12,37 +12,39 @@ module APB_GPIO (APB_RESET_n,APB_CLK,APB_PSEL,APB_PENABLE,APB_PADDR,APB_PWRITE,A
 	integer i ,j,k,l,c=0; //iteration variables for the "for" loop statements
 	 
 
+
+
 	//APB write to Mode register
-  always @(posedge PCLK,negedge PRESETn)
-    if      (!PRESETn) begin
+  always @(posedge APB_CLK,negedge APB_PRESET_n)
+    if      (!APB_PRESET_n) begin
 	mode_reg <= {PDATA_SIZE{1'b0}};
-	PREADY <= 1'b0 ;
+	APB_PREADY <= 1'b0 ;
 	end
-    else if (PSEL & PENABLE & PWRITE & (PADDR == MODE)) begin
-			mode_reg[7:0] = PSTRB[0] ? PWDATA[7:0] : mode_reg[7:0];
-			mode_reg[15:8] = PSTRB[1] ? PWDATA[15:8] : mode_reg[15:8];
-			mode_reg[23:16] = PSTRB[2] ? PWDATA[23:16] : mode_reg[23:16];
-			mode_reg[31:24] = PSTRB[3] ? PWDATA[31:24] : mode_reg[31:24];
+    else if (APB_PSEL & APB_PENABLE & APB_PWRITE & (APB_PADDR == MODE)) begin
+			mode_reg[7:0] = APB_PSTRB[0] ? APB_PWDATA[7:0] : mode_reg[7:0];
+			mode_reg[15:8] = APB_PSTRB[1] ? APB_PWDATA[15:8] : mode_reg[15:8];
+			mode_reg[23:16] = APB_PSTRB[2] ? APB_PWDATA[23:16] : mode_reg[23:16];
+			mode_reg[31:24] = APB_PSTRB[3] ? APB_PWDATA[31:24] : mode_reg[31:24];
 		end	
 
-	always @(posedge PCLK,negedge PRESETn)
-		if      (!PRESETn ) dir_reg <= {PDATA_SIZE{1'b0}};
-		else if (PSEL & PENABLE & PWRITE & (PADDR == DIRECTION)) begin
-			dir_reg[7:0] = PSTRB[0] ? PWDATA[7:0] : dir_reg[7:0];
-			dir_reg[15:8] = PSTRB[1] ? PWDATA[15:8] : dir_reg[15:8];
-			dir_reg[23:16] = PSTRB[2] ? PWDATA[23:16] : dir_reg[23:16];
-			dir_reg[31:24] = PSTRB[3] ? PWDATA[31:24] : dir_reg[31:24];
-			PREADY <= 1'b1 ;
+	always @(posedge APB_CLK,negedge APB_PRESET_n)
+		if      (!APB_PRESET_n ) dir_reg <= {PDATA_SIZE{1'b0}};
+		else if (APB_PSEL & APB_PENABLE & APB_PWRITE & (APB_PADDR == DIRECTION)) begin
+			dir_reg[7:0] = APB_PSTRB[0] ? APB_PWDATA[7:0] : dir_reg[7:0];
+			dir_reg[15:8] = APB_PSTRB[1] ? APB_PWDATA[15:8] : dir_reg[15:8];
+			dir_reg[23:16] = APB_PSTRB[2] ? APB_PWDATA[23:16] : dir_reg[23:16];
+			dir_reg[31:24] = APB_PSTRB[3] ? APB_PWDATA[31:24] : dir_reg[31:24];
+			APB_PREADY <= 1'b1 ;
 		end	
 		
-	always @(posedge PCLK,negedge PRESETn) 
-    if      (!PRESETn ) out_reg <= {PDATA_SIZE{1'b0}};
-    else if ( (PSEL & PENABLE & PWRITE  &(PADDR == OUTPUT)) || (PSEL & PENABLE & PWRITE  &(PADDR == INPUT))) begin
-			if(PREADY) begin
-				out_reg[7:0] = PSTRB[0] ? PWDATA[7:0] : out_reg[7:0];
-				out_reg[15:8] = PSTRB[1] ? PWDATA[15:8] : out_reg[15:8];
-				out_reg[23:16] = PSTRB[2] ? PWDATA[23:16] : out_reg[23:16];
-				out_reg[31:24] = PSTRB[3] ? PWDATA[31:24] : out_reg[31:24];
+	always @(posedge APB_CLK,negedge APB_PRESET_n) 
+    if      (!APB_PRESET_n ) out_reg <= {PDATA_SIZE{1'b0}};
+    else if ( (APB_PSEL & APB_PENABLE & APB_PWRITE  &(APB_PADDR == OUTPUT)) || (APB_PSEL & APB_PENABLE & APB_PWRITE  &(APB_PADDR == INPUT))) begin
+			if(APB_PREADY) begin
+				out_reg[7:0] = APB_PSTRB[0] ? APB_PWDATA[7:0] : out_reg[7:0];
+				out_reg[15:8] = APB_PSTRB[1] ? APB_PWDATA[15:8] : out_reg[15:8];
+				out_reg[23:16] = APB_PSTRB[2] ? APB_PWDATA[23:16] : out_reg[23:16];
+				out_reg[31:24] = APB_PSTRB[3] ? APB_PWDATA[31:24] : out_reg[31:24];
 			end
 			else
 				out_reg<= 32'bz	;
@@ -51,24 +53,24 @@ module APB_GPIO (APB_RESET_n,APB_CLK,APB_PSEL,APB_PENABLE,APB_PADDR,APB_PWRITE,A
 	  /*
    * APB Reads
    */
-  always @(posedge PCLK)
-    case (PADDR)
-	  MODE     : PRDATA <= mode_reg;	
-      DIRECTION: PRDATA <= dir_reg;
-      OUTPUT   : PRDATA <= out_reg;
+  always @(posedge APB_CLK)
+    case (APB_PADDR)
+	  MODE     : APB_PRDATA <= mode_reg;	
+      DIRECTION: APB_PRDATA <= dir_reg;
+      OUTPUT   : APB_PRDATA <= out_reg;
       INPUT    :
-		if((PSEL & PENABLE & ~PWRITE & PREADY)) 
-			PRDATA <= in_reg; //add condition
+		if((APB_PSEL & APB_PENABLE & ~APB_PWRITE & APB_PREADY)) 
+			APB_PRDATA <= in_reg; //add condition
 	
-      default  : PRDATA <= {PDATA_SIZE{1'b0}};
+      default  : APB_PRDATA <= {PDATA_SIZE{1'b0}};
     endcase	
 	
-	 always @(posedge PCLK)
+	 always @(posedge APB_CLK)
     for ( k=0; k<INPUT_STAGES; k = k +1)
-       if (k==0) input_regs[k] <= gpio_i;
+       if (k==0) input_regs[k] <= GPIO_i;
        else      input_regs[k] <= input_regs[k-1];
 
-  always @(posedge PCLK)
+  always @(posedge APB_CLK)
     in_reg <= input_regs[INPUT_STAGES-1];
     //my modification
 	
@@ -76,9 +78,9 @@ module APB_GPIO (APB_RESET_n,APB_CLK,APB_PSEL,APB_PENABLE,APB_PADDR,APB_PWRITE,A
 	 // mode
   // 0=push-pull    drive out_reg value onto transmitter input
   // 1=open-drain   always drive '0' onto transmitter
-  always @(posedge PCLK)
+  always @(posedge APB_CLK)
     for (i=0; i<PDATA_SIZE; i = i +1)
-      gpio_o[i] <= mode_reg[i] ? 1'b0 : out_reg[i];
+      GPIO_o[i] <= mode_reg[i] ? 1'b0 : out_reg[i];
 
 
   // direction  mode          out_reg
@@ -86,7 +88,7 @@ module APB_GPIO (APB_RESET_n,APB_CLK,APB_PSEL,APB_PENABLE,APB_PADDR,APB_PWRITE,A
   // 1=output   0=push-pull            always enable transmitter
   //            1=open-drain  1=Hi-Z   disable transmitter
   //                          0=low    enable transmitter
-  always @(posedge PCLK)
+  always @(posedge APB_CLK)
     for (j=0; j<PDATA_SIZE; j = j +1 )
-      gpio_oe[j] <= dir_reg[j] & ~(mode_reg[j] ? out_reg[j] : 1'b0)	;
+      GPIO_oe[j] <= dir_reg[j] & ~(mode_reg[j] ? out_reg[j] : 1'b0)	;
 endmodule	
